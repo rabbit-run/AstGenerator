9c929d9124f082906fa07e81fa727875af79f43d
maizehua <mai.zehua@gmail.com>
2011-10-26 13:55:15 -0400

    Add hello

---
+++ ASTgenerator/src/HelloGit.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+
+/**
+ * @version    
+ * @author     Zehua Mai
+ *
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
